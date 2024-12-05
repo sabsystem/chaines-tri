15858da36ccb1a87fe44abbeef3c499760421cad
RIN4D <rinad.stn@gmail.com>
2024-12-05 16:58:11 +0100

    Patched bugs

--- channels_console/static/scripts/clients.js
+++ channels_console/static/scripts/clients.js
@@ -47,20 +47,15 @@ async function supprimer_client(nom_client) {
 async function ouvrir_formulaire_chaines(id_formulaire, clientNom) {
     ouvrir_formulaire(id_formulaire);
 
-    const elementNomClient = document.getElementById("formulaire-chaine");
-    elementNomClient.textContent = clientNom;
-
-    const elementListeChaines = document.getElementById("liste-chaines");
-    const selecteursChaines = elementListeChaines.querySelectorAll("input[type='checkbox']");
-
-    // On récupère les chaînes déjà associées à ce client
-    const chainesAssociees = await fetch(`/api/clients/${clientNom}/chaines`)
-        .then(response => response.json());
-
-    // On met à jour les cases à cocher des chaînes en fonction de celles déjà associées
-    for (const selecteur of selecteursChaines) {
-        selecteur.checked = chainesAssociees.includes(selecteur.id);
+    // Vérifie si le nom du client a bien été passé
+    if (!clientNom) {
+        console.error("Le nom du client n'a pas été passé correctement.");
+        return;
     }
+
+    const elementNomClient = document.getElementById("formulaire-client");
+
+    elementNomClient.textContent = clientNom;
 }
 
 // Fonction pour fermer le formulaire
@@ -72,3 +67,6 @@ function fermer_formulaire(id_formulaire) {
 function ouvrir_formulaire(id_formulaire) {
     document.getElementById(id_formulaire).style.display = 'block';
 }
+
+
+
